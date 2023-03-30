cd88938c27613dbe4d1e9641af4fafa10f614082
guillermo <guillermo.dv@gmail.com>
2023-03-30 12:59:37 -0300

    add some stuff

--- app/components/UserAvatar.js
+++ app/components/UserAvatar.js
@@ -1,15 +1,21 @@
 "use client";
-import { Avatar, Grid } from "@nextui-org/react";
+import { Avatar, Grid, Text } from "@nextui-org/react";
+import styles from "../page.module.css";
 
-export default function UserAvatar() {
+export default function UserAvatar({ title = "User" }) {
   return (
-    <Grid.Container gap={2}>
-      <Grid>
-        <Avatar squared src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-      </Grid>
-      <Grid>
-        <Avatar squared text="Junior" />
-      </Grid>
-    </Grid.Container>
+    <div className={styles.rightPosition}>
+      <Grid.Container gap={2}>
+        <Grid>
+          <Avatar
+            squared
+            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+          />
+        </Grid>
+        <Grid>
+          <Avatar squared text="Junior" />
+        </Grid>
+      </Grid.Container>
+    </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,29 +1,80 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import UserTable from "./UserTable";
 import users from "./services/users";
 import UserAvatar from "./components/UserAvatar";
+import { Button, Text, Input, Row, Checkbox } from "@nextui-org/react";
 
-const Home = () => (
-  <main className={styles.main}>
-    <div className={styles.userDiv}>
-      <UserAvatar />
-    </div>
-    <div className={styles.center}>
-      <Image
-        className={styles.logo}
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
-      <div className={styles.thirteen}>
-        <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-      </div>
-    </div>
-    <UserTable users={users} />
-  </main>
-);
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
+const theme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      primary: "#4ADE7B",
+      secondary: "#F9CB80",
+      error: "#FCC5D8",
+    },
+  },
+});
+
+const closeHandler = () => {
+  setVisible(false);
+  console.log("closed");
+};
+
+const Home = () => {
+  return (
+    <>
+      <NextUIProvider theme={theme}>
+        <main className={styles.main}>
+          <div className={styles.rightPosition}>
+            <UserAvatar />
+          </div>
+          <div className={styles.leftPosition}>
+            <Text size={16} weight="bold">
+              Users
+            </Text>
+          </div>
+          <div className={styles.rightPosition}>
+            <Input clearable underlined Placeholder="Search name" />
+          </div>
+          <div className={styles.center}>
+            <Image
+              className={styles.logo}
+              src="/next.svg"
+              alt="Next.js Logo"
+              width={180}
+              height={37}
+              priority
+            />
+            <div className={styles.thirteen}>
+              <Image
+                src="/thirteen.svg"
+                alt="13"
+                width={40}
+                height={31}
+                priority
+              />
+            </div>
+          </div>
+          <UserTable users={users} />
+          <div className={styles.rightPosition}>
+            <Button
+              size="sm"
+              auto
+              shadow
+              color="secondary"
+              onPress={() => null}
+            >
+              Create User
+            </Button>
+          </div>
+        </main>
+      </NextUIProvider>
+    </>
+  );
+};
 
 export default Home;
